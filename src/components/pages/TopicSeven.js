5f11634b142fc6b0b5b10ed6a3e25701c3e446e7
ANS <36783010+answebdev@users.noreply.github.com>
2022-09-27 18:32:05 -0700

    add toggle functionality

--- src/components/pages/TopicSeven.js
+++ src/components/pages/TopicSeven.js
@@ -4,6 +4,7 @@ import '../styles/TopicSeven.css';
 
 const TopicSeven = () => {
   const [isActive, setActive] = useState('false');
+  const [myStyle, setMyStyle] = useState(false);
   const [episode, setEpisodes] = useState([]);
   const url = `https://the-twilight-zone-api.vercel.app/episodes`;
 
@@ -28,6 +29,13 @@ const TopicSeven = () => {
     setActive(!isActive);
   };
 
+  const handleClick2 = (id) => {
+    setMyStyle((prevState) => ({
+      ...myStyle,
+      [id]: !prevState[id],
+    }));
+  };
+
   return (
     <Container>
       <div data-test='component-input'>
@@ -38,6 +46,16 @@ const TopicSeven = () => {
         <p>
           Useful resources:
           <ul>
+            <li>
+              Stack Overflow &nbsp;| &nbsp;
+              <a
+                href='https://stackoverflow.com/questions/71946724/how-to-change-style-of-one-element-in-map-using-react'
+                rel='noopener noreferrer'
+                target='_blank'
+              >
+                View
+              </a>
+            </li>
             <li>
               freeCodeCamp &nbsp;| &nbsp;
               <a
@@ -73,6 +91,10 @@ const TopicSeven = () => {
         <hr />
         <div>
           <h4>Variation 1</h4>
+          <p>
+            Here, when the button is clicked, all of the items are targeted and
+            the background color of all the items is changed.
+          </p>
           <div style={{ marginBottom: '0', display: 'flex', flexWrap: 'wrap' }}>
             {/* Display only the first 6 items: */}
             {episode.slice(0, 6).map((item) => {
@@ -94,26 +116,105 @@ const TopicSeven = () => {
           </div>
         </div>
 
-        <br />
         <div>
-          <h4>Variation 2</h4>
+          <h4 style={{ marginTop: '40px' }}>Variation 2</h4>
+          <p>
+            Here, when the button is clicked, a single item is targeted and the
+            background color of only the item that is clicked is changed.
+          </p>
           <div style={{ marginBottom: '0', display: 'flex', flexWrap: 'wrap' }}>
             {/* Display only the first 6 items: */}
             {episode.slice(0, 6).map((item) => {
               return (
                 <div key={item.id} style={{ margin: '40px' }}>
-                  <Card style={{ width: '18rem' }}>
+                  <Card
+                    className={myStyle[`${item.id}`] ? 'style2' : 'style1'}
+
+                    // Alternative Way: Using state with 'style'.
+                    // style={{
+                    //   backgroundColor: myStyle[`${item.id}`]
+                    //     ? 'tomato'
+                    //     : '#ffffff',
+                    //   width: myStyle[`${item.id}`] ? '18rem' : '18rem',
+                    // }}
+                  >
                     <Card.Img variant='top' src={item.img} />
                     <Card.Body>
                       <Card.Title>{item.title}</Card.Title>
                       <Card.Text>{item.storyline}</Card.Text>
-                      <Button variant='primary'>Click Me</Button>
+                      <Button
+                        variant='primary'
+                        onClick={() => handleClick2(item.id)}
+                      >
+                        Click Me
+                      </Button>
                     </Card.Body>
                   </Card>
                 </div>
               );
             })}
           </div>
+        </div>
+      </div>
+
+      <div>
+        <h4 style={{ marginTop: '40px' }}>Variation 3</h4>
+        <p>
+          Here, when the button is clicked, a single item is targeted and the
+          'Details' text of only the item that is clicked is toggled.
+        </p>
+        <div style={{ marginBottom: '0', display: 'flex', flexWrap: 'wrap' }}>
+          {/* Display only the first 6 items: */}
+          {episode.slice(0, 6).map((item) => {
+            return (
+              <div key={item.id} style={{ margin: '40px' }}>
+                <Card className='style1'>
+                  <Card.Img variant='top' src={item.img} />
+                  <Card.Body>
+                    <Card.Title>{item.title}</Card.Title>
+                    <Card.Text>{item.storyline}</Card.Text>
+                    <div>
+                      <p>
+                        <strong>Details</strong>
+                        <div
+                          onClick={() => handleClick2(item.id)}
+                          style={{ float: 'right', cursor: 'pointer' }}
+                        >
+                          <span>
+                            {myStyle[`${item.id}`] ? (
+                              <i class='fa-solid fa-minus'></i>
+                            ) : (
+                              <i class='fa-solid fa-plus'></i>
+                            )}
+                          </span>
+                        </div>
+                      </p>
+                      <p
+                        className={myStyle[`${item.id}`] ? 'style3' : 'style4'}
+                      >
+                        <div style={{ lineHeight: '2' }}>
+                          <p style={{ width: '300px', lineHeight: '1.5' }}>
+                            <strong>Season: </strong> {item.season}
+                            <br />
+                            <strong>Episode: </strong> {item.episode}
+                            <br />
+                            <strong>Directed by: </strong>
+                            {item.directed_by}
+                            <br />
+                            <strong>Written by: </strong>
+                            {item.written_by}
+                            <br />
+                            <strong>Original Air Date: </strong>
+                            {item.air_date}
+                          </p>
+                        </div>
+                      </p>
+                    </div>
+                  </Card.Body>
+                </Card>
+              </div>
+            );
+          })}
         </div>
       </div>
     </Container>
